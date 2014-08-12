f61aafc90be651cf7e74d8612c80218e01aeaf36
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2014-08-12 17:19:50 -0400

    hacky fix for inverted bounding box vertices in LOVE
    See http://esotericsoftware.com/forum/viewtopic.php?f=9&t=3153

--- spine-love/spine-love/spine.lua
+++ spine-love/spine-love/spine.lua
@@ -49,6 +49,41 @@ spine.EventData = require "spine-lua.EventData"
 spine.Event = require "spine-lua.Event"
 spine.SkeletonBounds = require "spine-lua.SkeletonBounds"
 
+-- FIXME: Copy-pasted from spine-lua/BoundingBoxAttachment.lua with a tiny
+--        change. There must be a cleaner way.
+spine.BoundingBoxAttachment = require "spine-lua.BoundingBoxAttachment"
+function spine.BoundingBoxAttachment.new (name)
+	if not name then error("name cannot be nil", 2) end
+
+	local self = {
+		name = name,
+		type = spine.AttachmentType.boundingbox,
+		vertices = {}
+	}
+
+	function self:computeWorldVertices (x, y, bone, worldVertices)
+		x = x + bone.worldX
+		y = y + bone.worldY
+		local m00 = bone.m00
+		local m01 = bone.m01
+		local m10 = bone.m10
+		local m11 = bone.m11
+		local vertices = self.vertices
+		local count = #vertices
+		for i = 1, count, 2 do
+			local px = vertices[i]
+			local py = vertices[i + 1]
+
+			-- Changed +py to -py to account for LOVE's inverted y axis
+			worldVertices[i] = px * m00 - py * m01 + x
+			worldVertices[i + 1] = px * m10 - py * m11 + y
+		end
+	end
+
+	return self
+end
+
+
 spine.utils.readFile = function (fileName, base)
 	local path = fileName
 	if base then path = base .. '/' .. path end
